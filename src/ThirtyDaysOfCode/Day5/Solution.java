b9402dd380f97aa30e50053d5fa77573db05ddeb
Anh Nguyen <a.nguyen0208@gmail.com>
2021-02-26 17:44:04 -0600

    comment

--- src/ThirtyDaysOfCode/Day5/Solution.java
+++ src/ThirtyDaysOfCode/Day5/Solution.java
@@ -17,10 +17,12 @@ public class Solution {
         System.out.println("Enter number multiplication table number");
         int n = scanner.nextInt();
         scanner.skip( "(\r\n|[\n\r\u2028\u2029\u0085])?" );
+        //   Constraints 2 ≤ n ≤ 20
         if((n < 2) || (n > 20)) {
             System.out.println( "Invalid input, between 2-20" );
         } else{
             int result;
+            //  loop start with 1 and end with 10
             for (int i = 1; i <= 10 ; i++){
                 result = (n*i);
                 System.out.println(n + " x " + i + " = "+ result );
